4f0e7ec93d37be99f20c3ff3fea9fa8000487136
SpaceRanch Dev <91358513+SpaceRanchDev@users.noreply.github.com>
2022-09-18 12:12:28 -0700

    Update Footer.tsx

--- containers/Marketplace/components/Footer.tsx
+++ containers/Marketplace/components/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
       <div className="flex justify-between items-center pt-4 sm:pt-0 pb-4 sm:pb-0">
       <img src="logo-x.png" className="flex items-center sm:ml-5" style={{width: '30px'}} />
         <h1 className="font-light text-2xl uppercase p-2 sm:p-4 border-gray-100 hidden sm:block">
-          SpaceRanch DAO
+          SpaceDAO
         </h1>
       </div>
       {/** login/logout with wallet */}
       <div className="flex items-center mr-4">
-        <a href="https://spaceranch.gitbook.io/documentation" target="_blank">Learn more with our Gitbook</a>
+        <a href="https://spaceranch.gitbook.io/documentation" target="_blank">Documentation</a>
       </div>
     </nav>
   )
