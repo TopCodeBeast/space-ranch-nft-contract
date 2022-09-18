5bbd83ce6b4e7b470aefa77bf88f9337f5c54ad1
SpaceRanch Dev <91358513+SpaceRanchDev@users.noreply.github.com>
2022-09-18 12:11:05 -0700

    Update HeroSection.tsx

--- containers/Marketplace/components/HeroSection.tsx
+++ containers/Marketplace/components/HeroSection.tsx
@@ -12,16 +12,14 @@ const HeroSection = () => {
 
         <div className="pl-4 pr-4 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4 my-12 text-white">
           <div className="text-xl text-white mt-1 font-bold">
-          &#62;&#62;&#62; The Journey Begins Today.<br />
-            Join us on our mission to collaboratively build the future of space exploration and asset managment to Wyoming, under our DAO LLC.
+          &#62;&#62;&#62; Live Demo on NEAR Testnet <br />
+            Join us on our mission to collaboratively build the future of space exploration in Wyoming.
           </div>
           <div className="">
-            The <span className="text-white">SpaceRanch NFT Membership Fundraiser</span> is a mechanism to raise capital through tiered membership, which will enable participants to engage with our <span className="text-white">web3 applications</span> built for the <span className="text-white">SpaceRanch ecosystem</span>, as well as access to the physical properties and digital immersive environments.
+            <span className="text-white">The SpaceRanch Membership is a mechanism for governance and applications built for the SpaceRanch ecosystem, which includes access to the physical properties and digital immersive environments.</span>
           </div>
           <div className="">
-            A succesful program will generate <span className="text-white">$25 million</span>, which will be the necessary amount of funding to meet capital expendiatures for the purchase and development of the SpaceRanch.
-
-            Up to 10% of the SPCR token supply will be distributed to the <span className="text-white">SpaceRanch NFT Members</span> proportional to rank.
+            <span className="text-white">There are five ranks which represent different teirs of opportunities and responsibilities within the organization. Climb the ranks by completing bounties and participating in the SpaceRanch.</span> 
           </div>
         </div>
       </div>
